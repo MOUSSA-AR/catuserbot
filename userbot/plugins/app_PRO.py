56a5e2e1101ce217a5143de18aea5332f4e9bca9
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2021-07-16 02:34:22 +0300

    Update and rename app.py to app_PRO.py

--- userbot/plugins/app_PRO.py
+++ userbot/plugins/app_PRO.py
@@ -1,7 +1,7 @@
 """
-Fetch App Details from Playstore.
-.app <app_name> to fetch app details.
-  © [cHAuHaN](http://t.me/amnd33p)
+.لإحضار تفاصيل التطبيق من متجر بلاي
+.استخدم الأمر .تطبيق<اسم التطبيق> لجلب تفاصيل عن التطبيق
+  © [معرف المطور](http://t.me/u_5_1)
 """
 
 import bs4
@@ -13,8 +13,8 @@ plugin_category = "utils"
 
 
 @catub.cat_cmd(
-    pattern="app ([\s\S]*)",
-    command=("app", plugin_category),
+    pattern="تطبيق ([\s\S]*)",
+    command=("تطبيق", plugin_category),
     info={
         "header": "To search any app in playstore",
         "description": "Searches the app in the playstore and provides the link to the app in playstore and fetchs app details",
@@ -24,7 +24,7 @@ plugin_category = "utils"
 async def app_search(event):
     "To search any app in playstore."
     app_name = event.pattern_match.group(1)
-    event = await edit_or_reply(event, "`Searching!..`")
+    event = await edit_or_reply(event, "`يتم البحث...!`")
     try:
         remove_space = app_name.split(" ")
         final_name = "+".join(remove_space)
@@ -61,16 +61,16 @@ async def app_search(event):
             .findNext("div", "uzcko")
             .img["data-src"]
         )
-        app_details = "<a href='" + app_icon + "'>📲&#8203;</a>"
+        app_details = "<a href='" + app_icon + "'〽️التطبيق :&#8203;</a>"
         app_details += " <b>" + app_name + "</b>"
         app_details += (
-            "\n\n<code>Developer :</code> <a href='"
+            "\n\n<code>👨‍💻المطور :</code> <a href='"
             + app_dev_link
             + "'>"
             + app_dev
             + "</a>"
         )
-        app_details += "\n<code>Rating :</code> " + app_rating.replace(
+        app_details += "\n<code>🌟تقييم التطبيق :</code> " + app_rating.replace(
             "Rated ", "⭐ "
         ).replace(" out of ", "/").replace(" stars", "", 1).replace(
             " stars", "⭐ "
@@ -78,13 +78,13 @@ async def app_search(event):
             "five", "5"
         )
         app_details += (
-            "\n<code>Features :</code> <a href='"
+            "\n<code>💎ميزات التطبيق :</code> <a href='"
             + app_link
-            + "'>View in Play Store</a>"
+            + "'>مشاهدة على متجر بلاي</a>"
         )
         app_details += f"\n\n===> {ALIVE_NAME} <==="
         await event.edit(app_details, link_preview=True, parse_mode="HTML")
     except IndexError:
-        await event.edit("No result found in search. Please enter **Valid app name**")
+        await event.edit("تعذر العثور على التطبيق. يرجى إدخال **اسم تطبيق صالح**")
     except Exception as err:
-        await event.edit("Exception Occured:- " + str(err))
+        await event.edit("حدث استثناء:- " + str(err))
