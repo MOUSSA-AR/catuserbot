c4fc33fdd3d63421a6ba4d50c58473dc5d73eefc
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2021-07-21 12:39:43 +0300

    Update android.py

--- userbot/plugins/android.py
+++ userbot/plugins/android.py
@@ -12,10 +12,10 @@ plugin_category = "extra"
 
 
 @catub.cat_cmd(
-    pattern="magisk$",
-    command=("magisk", plugin_category),
+    pattern="سحر$",
+    command=("سحر", plugin_category),
     info={
-        "header": "To Get latest Magisk releases",
+        "header": "للحصول على احدث اصدارات السحر",
         "usage": "{tr}magisk",
     },
 )
@@ -23,23 +23,23 @@ async def kakashi(event):
     "Get latest Magisk releases"
     magisk_repo = "https://raw.githubusercontent.com/topjohnwu/magisk-files/"
     magisk_dict = {
-        "⦁ **Stable**": magisk_repo + "master/stable.json",
-        "⦁ **Beta**": magisk_repo + "master/beta.json",
-        "⦁ **Canary**": magisk_repo + "master/canary.json",
+        "⦁ **مستقر**": magisk_repo + "master/stable.json",
+        "⦁ **بيتا**": magisk_repo + "master/beta.json",
+        "⦁ **كناري**": magisk_repo + "master/canary.json",
     }
-    releases = "**Latest Magisk Releases**\n\n"
+    releases = "**احدث اصدارات السحر**\n\n"
     for name, release_url in magisk_dict.items():
         data = get(release_url).json()
         releases += (
-            f'{name}: [APK v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
-            f'[Changelog]({data["magisk"]["note"]})\n'
+            f'{name}: [التطبيق v{data["magisk"]["version"]}]({data["magisk"]["link"]}) | '
+            f'[التغييرات]({data["magisk"]["note"]})\n'
         )
     await edit_or_reply(event, releases)
 
 
 @catub.cat_cmd(
-    pattern="device(?: |$)(\S*)",
-    command=("device", plugin_category),
+    pattern="جهاز(?: |$)(\S*)",
+    command=("جهاز", plugin_category),
     info={
         "header": "To get android device name/model from its codename",
         "usage": "{tr}device <codename>",
@@ -54,7 +54,7 @@ async def device_info(event):
         if textx:
             codename = textx.text
         else:
-            return await edit_delete(event, "`Usage: .device <codename> / <model>`")
+            return await edit_delete(event, "`الاستخدام: .جهاز <اسم الكود> / <نوع الجهاز>`")
     data = json.loads(
         get(
             "https://raw.githubusercontent.com/androidtrackers/"
@@ -63,21 +63,21 @@ async def device_info(event):
     )
     results = data.get(codename)
     if results:
-        reply = f"**Search results for {codename}**:\n\n"
+        reply = f"**نتائج البحث عن {codename}**:\n\n"
         for item in results:
             reply += (
-                f"**Brand**: {item['brand']}\n"
-                f"**Name**: {item['name']}\n"
-                f"**Model**: {item['model']}\n\n"
+                f"**الشركة المصنعة**: {item['brand']}\n"
+                f"**الاسم**: {item['name']}\n"
+                f"**النموزج**: {item['model']}\n\n"
             )
     else:
-        reply = f"`Couldn't find info about {codename}!`\n"
+        reply = f"`تعذر العثور على معلومات حول {codename}!`\n"
     await edit_or_reply(event, reply)
 
 
 @catub.cat_cmd(
-    pattern="codename(?: |)([\S]*)(?: |)([\s\S]*)",
-    command=("codename", plugin_category),
+    pattern="كود(?: |)([\S]*)(?: |)([\s\S]*)",
+    command=("كود", plugin_category),
     info={
         "header": "To Search for android device codename",
         "usage": "{tr}codename <brand> <device>",
@@ -95,7 +95,7 @@ async def codename_info(event):
         brand = textx.text.split(" ")[0]
         device = " ".join(textx.text.split(" ")[1:])
     else:
-        return await edit_delete(event, "`Usage: .codename <brand> <device>`")
+        return await edit_delete(event, "`الاستخدام: .كود <الشركة المصنعة> <الجهاز>`")
 
     data = json.loads(
         get(
@@ -111,23 +111,23 @@ async def codename_info(event):
         if i["name"].lower() == device.lower() or i["model"].lower() == device.lower()
     ]
     if results:
-        reply = f"**Search results for {brand} {device}**:\n\n"
+        reply = f"**نتائج البحث عن {brand} {device}**:\n\n"
         if len(results) > 8:
             results = results[:8]
         for item in results:
             reply += (
-                f"**Device**: {item['device']}\n"
-                f"**Name**: {item['name']}\n"
-                f"**Model**: {item['model']}\n\n"
+                f"**الجهاز**: {item['device']}\n"
+                f"**الأسم**: {item['name']}\n"
+                f"**النموذج**: {item['model']}\n\n"
             )
     else:
-        reply = f"`Couldn't find {device} codename!`\n"
+        reply = f"`لا يمكنني العثور على {device} الجهاز من الكود!`\n"
     await edit_or_reply(event, reply)
 
 
 @catub.cat_cmd(
-    pattern="specs(?: |)([\S]*)(?: |)([\s\S]*)",
-    command=("specs", plugin_category),
+    pattern="حول(?: |)([\S]*)(?: |)([\s\S]*)",
+    command=("حول", plugin_category),
     info={
         "header": "To Get info about android device .",
         "usage": "{tr}specs",
@@ -145,7 +145,7 @@ async def devices_specifications(event):
         brand = textx.text.split(" ")[0]
         device = " ".join(textx.text.split(" ")[1:])
     else:
-        return await edit_delete(event, "`Usage: .specs <brand> <device>`")
+        return await edit_delete(event, "`الاستخدام: .حول <الشركة المصنعة> <الجهاز>`")
     all_brands = (
         BeautifulSoup(
             get("https://www.devicespecifications.com/en/brand-more").content, "lxml"
@@ -159,7 +159,7 @@ async def devices_specifications(event):
             i["href"] for i in all_brands if brand == i.text.strip().lower()
         ][0]
     except IndexError:
-        return await edit_delete(event, f"`{brand} is unknown brand!`")
+        return await edit_delete(event, f"`{brand} علامة تجارية غير معروفة!`")
     devices = BeautifulSoup(get(brand_page_url).content, "lxml").findAll(
         "div", {"class": "model-listing-container-80"}
     )
@@ -193,8 +193,8 @@ async def devices_specifications(event):
 
 
 @catub.cat_cmd(
-    pattern="twrp(?: |$)(\S*)",
-    command=("twrp", plugin_category),
+    pattern="موقع(?: |$)(\S*)",
+    command=("موقع", plugin_category),
     info={
         "header": "To Get latest twrp download links for android device.",
         "usage": "{tr}twrp <codename>",
@@ -210,10 +210,10 @@ async def twrp(event):
     elif textx:
         device = textx.text.split(" ")[0]
     else:
-        return await edit_delete(event, "`Usage: .twrp <codename>`")
+        return await edit_delete(event, "`الاستخدام: .موقع <الكود>`")
     url = get(f"https://dl.twrp.me/{device}/")
     if url.status_code == 404:
-        reply = f"`Couldn't find twrp downloads for {device}!`\n"
+        reply = f"`تعذر العثور على تنزيلات الموقع ل {device}!`\n"
         return await edit_delete(event, reply)
     page = BeautifulSoup(url.content, "lxml")
     download = page.find("table").find("tr").find("a")
@@ -222,8 +222,8 @@ async def twrp(event):
     size = page.find("span", {"class": "filesize"}).text
     date = page.find("em").text.strip()
     reply = (
-        f"**Latest TWRP for {device}:**\n"
+        f"**احدث موقع ل {device}:**\n"
         f"[{dl_file}]({dl_link}) - __{size}__\n"
-        f"**Updated:** __{date}__\n"
+        f"**محدث:** __{date}__\n"
     )
     await edit_or_reply(event, reply)
