d325e12922e6ba0bcc06919e458c699ce9d6ec9f
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2021-07-23 23:06:10 +0300

    Update and rename blacklistchats.py to الدردشة_السوداء_برو.py

--- userbot/plugins/الدردشة_السوداء_برو.py
+++ userbot/plugins/الدردشة_السوداء_برو.py
@@ -16,8 +16,8 @@ LOGS = logging.getLogger(__name__)
 
 
 @catub.cat_cmd(
-    pattern="chatblacklist (on|off)$",
-    command=("chatblacklist", plugin_category),
+    pattern="الدردشة السوداء (on|off)$",
+    command=("الدردشة السوداء", plugin_category),
     info={
         "header": "To enable and disable chats blacklist.",
         "description": "If you turn this on, then your userbot won't work on the chats stored\
@@ -32,12 +32,12 @@ async def chat_blacklist(event):
     blkchats = blacklist_chats_list()
     if input_str == "on":
         if gvarstatus("blacklist_chats") is not None:
-            return await edit_delete(event, "__Already it was turned on.__")
+            return await edit_delete(event, "__تم تمكينه بالفعل.__")
         addgvar("blacklist_chats", "true")
-        text = "__From now on, your CatUserbot doesn't work in the chats stored in database.__"
+        text = "__من الآن فصاعدًا ، لن يعمل البوت برو في الدردشات المخزنة في قاعدة البيانات.__"
         if len(blkchats) != 0:
             text += (
-                "**Bot is reloading to apply the changes. Please wait for a minute**"
+                "**يقوم البوت بإعادة التحميل لتطبيق التغييرات.  من فضلك انتظر دقيقة**"
             )
             msg = await edit_or_reply(
                 event,
@@ -45,17 +45,17 @@ async def chat_blacklist(event):
             )
             return await event.client.reload(msg)
         else:
-            text += "**You haven't added any chat to blacklist.**"
+            text += "**لم تقم بإضافة أي دردشة إلى القائمة السوداء.**"
             return await edit_or_reply(
                 event,
                 text,
             )
     if gvarstatus("blacklist_chats") is not None:
         delgvar("blacklist_chats")
-        text = "__Your CatUserbot is as free as a bird.It works in Every Chat .__"
+        text = "__إن البوت برو الخاص بك بوضع ممتاز ، وهو يعمل في كل محادثة.__"
         if len(blkchats) != 0:
             text += (
-                "**Bot is reloading to apply the changes. Please wait for a minute**"
+                "**يقوم البوت بإعادة التحميل لتطبيق التغييرات.  من فضلك انتظر دقيقة**"
             )
             msg = await edit_or_reply(
                 event,
@@ -63,17 +63,17 @@ async def chat_blacklist(event):
             )
             return await event.client.reload(msg)
         else:
-            text += "**You haven't added any chat to blacklist.**"
+            text += "**لم تقم بإضافة أي دردشة إلى القائمة السوداء.**"
             return await edit_or_reply(
                 event,
                 text,
             )
-    await edit_delete(event, "It was turned off already")
+    await edit_delete(event, "تم إيقاف تشغيله بالفعل")
 
 
 @catub.cat_cmd(
-    pattern="addblkchat(s)?(?:\s|$)([\s\S]*)",
-    command=("addblkchat", plugin_category),
+    pattern="اضف دردشة(s)?(?:\s|$)([\s\S]*)",
+    command=("اضف دردشة", plugin_category),
     info={
         "header": "To add chats to blacklist.",
         "description": "to add the chats to database so your bot doesn't work in\
@@ -101,7 +101,7 @@ async def add_blacklist_chat(event):
             try:
                 chatid = int(chatid.strip())
                 if chatid in blkchats:
-                    errors += f"**While adding the {chatid}** - __This chat has already been blacklisted__\n"
+                    errors += f"**اثناء اضافة الدردشة {chatid}** - __تم بالفعل وضع هذه الدردشة في القائمة السوداء__\n"
                     continue
                 chat = await event.client.get_entity(chatid)
                 date = str(datetime.now().strftime("%B %d, %Y"))
@@ -113,16 +113,16 @@ async def add_blacklist_chat(event):
                 }
                 blacklistchats[str(chat.id)] = chatdata
                 result += (
-                    f"successfully added {get_display_name(chat)} to blacklist chats.\n"
+                    f"تم اضافة {get_display_name(chat)} إلى القائمة السوداء للدردشات.\n"
                 )
             except Exception as e:
-                errors += f"**While adding the {chatid}** - __{str(e)}__\n"
+                errors += f"**اثناء اضافة الدردشة {chatid}** - __{str(e)}__\n"
     else:
         chat = await event.get_chat()
         try:
             chatid = chat.id
             if chatid in blkchats:
-                errors += f"**While adding the {chatid}** - __This chat has already been blacklisted__\n"
+                errors += f"**اثناء اضافة الدردشة {chatid}**__تم بالفعل وضع هذه الدردشة في القائمة السوداء__\n"
             else:
                 date = str(datetime.now().strftime("%B %d, %Y"))
                 chatdata = {
@@ -133,26 +133,26 @@ async def add_blacklist_chat(event):
                 }
                 blacklistchats[str(chat.id)] = chatdata
                 result += (
-                    f"successfully added {get_display_name(chat)} to blacklist chats.\n"
+                    f"تم اضافة {get_display_name(chat)} إلى القائمة السوداء للدردشات.\n"
                 )
         except Exception as e:
-            errors += f"**While adding the {chatid}** - __{str(e)}__\n"
+            errors += f"**جاري اضافة الدردشة {chatid}** - __{str(e)}__\n"
     sql.del_collection("blacklist_chats_list")
     sql.add_collection("blacklist_chats_list", blacklistchats, {})
     output = ""
     if result != "":
-        output += f"**Success:**\n{result}\n"
+        output += f"**نجاح:**\n{result}\n"
     if errors != "":
-        output += f"**Error:**\n{errors}\n"
+        output += f"**فشل:**\n{errors}\n"
     if result != "":
-        output += "**Bot is reloading to apply the changes. Please wait for a minute**"
+        output += "**يقوم البوت بإعادة التحميل لتطبيق التغييرات.  من فضلك انتظر دقيقة**"
     msg = await edit_or_reply(event, output)
     await event.client.reload(msg)
 
 
 @catub.cat_cmd(
-    pattern="rmblkchat(s)?(?:\s|$)([\s\S]*)",
-    command=("rmblkchat", plugin_category),
+    pattern="اازالة دردشة(s)?(?:\s|$)([\s\S]*)",
+    command=("ازالة دردشة", plugin_category),
     info={
         "header": "To remove chats to blacklist.",
         "description": "to remove the chats from database so your bot will work in\
@@ -183,12 +183,12 @@ async def add_blacklist_chat(event):
                     chatname = blacklistchats[str(chatid)]["chat_name"]
                     del blacklistchats[str(chatid)]
                     result += (
-                        f"successfully removed {chatname} from blacklisted chats.\n"
+                        f"تم ازالة {chatname} من القائمة السوداء للدردشات.\n"
                     )
                 else:
-                    errors += f"the given id {chatid} doesn't exists in your database. That is it hasn't been blacklisted.\n"
+                    errors += f"العضو {chatid} لا يوجد في قاعدة البيانات الخاصة بك.  هذا العضو لم يتم إدراجه في القائمة السوداء.\n"
             except Exception as e:
-                errors += f"**While removing the {chatid}** - __{str(e)}__\n"
+                errors += f"**اثناء ازالة الدردشة {chatid}** - __{str(e)}__\n"
     else:
         chat = await event.get_chat()
         try:
@@ -196,27 +196,27 @@ async def add_blacklist_chat(event):
             if chatid in blkchats:
                 chatname = blacklistchats[str(chatid)]["chat_name"]
                 del blacklistchats[str(chatid)]
-                result += f"successfully removed {chatname} from blacklisted chats.\n"
+                result += f"تم ازالة {chatname} من القائمة السوداء للدردشات.\n"
             else:
-                errors += f"the given id {chatid} doesn't exists in your database. That is it hasn't been blacklisted.\n"
+                errors += f"العضو {chatid} لا يوجد في قاعدة البيانات الخاصة بك.  هذا العضو لم يتم إدراجه في القائمة السوداء.\n"
         except Exception as e:
-            errors += f"**While removing the {chatid}** - __{str(e)}__\n"
+            errors += f"**اثناء ازالة العضو {chatid}** - __{str(e)}__\n"
     sql.del_collection("blacklist_chats_list")
     sql.add_collection("blacklist_chats_list", blacklistchats, {})
     output = ""
     if result != "":
-        output += f"**Success:**\n{result}\n"
+        output += f"**نجاح:**\n{result}\n"
     if errors != "":
-        output += f"**Error:**\n{errors}\n"
+        output += f"**فشل:**\n{errors}\n"
     if result != "":
-        output += "**Bot is reloading to apply the changes. Please wait for a minute**"
+        output += "**يقوم البوت بإعادة التحميل لتطبيق التغييرات.  من فضلك انتظر دقيقة**"
     msg = await edit_or_reply(event, output)
     await event.client.reload(msg)
 
 
 @catub.cat_cmd(
-    pattern="listblkchats$",
-    command=("listblkchats", plugin_category),
+    pattern="الدردشة السوداء$",
+    command=("الدردشة السوداء", plugin_category),
     info={
         "header": "To list all blacklisted chats.",
         "description": "Will show you the list of all blacklisted chats",
@@ -234,13 +234,13 @@ async def add_blacklist_chat(event):
         blacklistchats = {}
     if len(blkchats) == 0:
         return await edit_delete(
-            event, "__There are no blacklisted chats in your bot.__"
+            event, "__لا توجد محادثات في القائمة السوداء في البوت الخاص بك .__"
         )
-    result = "**The list of blacklisted chats are :**\n\n"
+    result = "**قائمة الدردشات المدرجة في القائمة السوداء هي :**\n\n"
     for chat in blkchats:
         result += f"☞ {blacklistchats[str(chat)]['chat_name']}\n"
-        result += f"**Chat Id :** `{chat}`\n"
-        username = blacklistchats[str(chat)]["chat_username"] or "__Private group__"
-        result += f"**Username :** {username}\n"
-        result += f"Added on {blacklistchats[str(chat)]['date']}\n\n"
+        result += f"**ايدي الدردشة :** `{chat}`\n"
+        username = blacklistchats[str(chat)]["chat_username"] or "__مجموعة خاصة__"
+        result += f"**معرف الدردشة :** {username}\n"
+        result += f"اضافة في {blacklistchats[str(chat)]['date']}\n\n"
     await edit_or_reply(event, result)
