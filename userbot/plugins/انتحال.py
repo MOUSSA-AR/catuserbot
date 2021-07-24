58a203dd3f6c103b5c00c4083be47848b32f5af3
MOUSSA-AR <86672022+MOUSSA-AR@users.noreply.github.com>
2021-07-24 15:18:52 +0300

    Update and rename clone.py to انتحال.py

--- userbot/plugins/انتحال.py
+++ userbot/plugins/انتحال.py
@@ -26,8 +26,8 @@ DEFAULTUSERBIO = (
 
 
 @catub.cat_cmd(
-    pattern="clone(?:\s|$)([\s\S]*)",
-    command=("clone", plugin_category),
+    pattern="انتحال(?:\s|$)([\s\S]*)",
+    command=("انتحال", plugin_category),
     info={
         "header": "To clone account of mentiond user or replied user",
         "usage": "{tr}clone <username/userid/reply>",
@@ -58,17 +58,17 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=user_bio))
     pfile = await event.client.upload_file(profile_pic)
     await event.client(functions.photos.UploadProfilePhotoRequest(pfile))
-    await edit_delete(event, "**LET US BE AS ONE**")
+    await edit_delete(event, "**لنكن كشخص واحد**")
     if BOTLOG:
         await event.client.send_message(
             BOTLOG_CHATID,
-            f"#CLONED\nsuccessfully cloned [{first_name}](tg://user?id={user_id })",
+            f"#انتحال\nتم انتحال شخصية [{first_name}](tg://user?id={user_id })",
         )
 
 
 @catub.cat_cmd(
-    pattern="revert$",
-    command=("revert", plugin_category),
+    pattern="اعادة$",
+    command=("اعادة", plugin_category),
     info={
         "header": "To revert back to your original name , bio and profile pic",
         "note": "For proper Functioning of this command you need to set AUTONAME and DEFAULT_BIO with your profile name and bio respectively.",
@@ -88,8 +88,8 @@ async def _(event):
     await event.client(functions.account.UpdateProfileRequest(about=bio))
     await event.client(functions.account.UpdateProfileRequest(first_name=name))
     await event.client(functions.account.UpdateProfileRequest(last_name=blank))
-    await edit_delete(event, "successfully reverted to your account back")
+    await edit_delete(event, "عاد بنجاح إلى حسابك مرة أخرى")
     if BOTLOG:
         await event.client.send_message(
-            BOTLOG_CHATID, f"#REVERT\nsuccessfully reverted back to your profile"
+            BOTLOG_CHATID, f"#اعادة\عاد بنجاح إلى حسابك"
         )
